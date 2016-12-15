37850663aa121fc7189111fc29e9dde1357a4e8c
andrewuebe <andrewuebe@gmail.com>
2016-12-15 15:47:07 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -5,6 +5,12 @@ function openNav() {
     document.getElementsByClassName("container")[0].style.opacity = "0.0";
 }
 
+/* Open when someone clicks on the span element */
+function openFeat() {
+    document.getElementById("myNav").style.opacity = "0.0";
+    document.getElementsByClassName("yay").style.opacity = "1.0";
+}
+
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.opacity = "0.0";
