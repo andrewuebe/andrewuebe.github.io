e8f1cc9ef849e3224ed68e30caf91e169478c88a
Andrew Uebelein <auebelei@skidmore.edu>
2017-09-05 16:11:58 -0500

    edit

--- nav.js
+++ nav.js
@@ -5,14 +5,21 @@ function openNav() {
     document.getElementsByClassName("container")[0].style.opacity = "0.0";
 }
 
+<<<<<<< Updated upstream
 /* Open when someone clicks on the span element */
 function openFeat() {
     document.getElementById("myNav").style.opacity = "0.0";
     document.getElementsByClassName("yay").style.opacity = "1.0";
 }
 
+=======
+>>>>>>> Stashed changes
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.opacity = "0.0";
     document.getElementsByClassName("container")[0].style.opacity = "1";
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
