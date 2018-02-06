2cac84b8fe40513a609ec544b171a406a73c974c
Andrew Uebelein <auebelei@skidmore.edu>
2018-02-05 22:50:15 -0600

    crystal ready!!!

--- nav.js
+++ nav.js
@@ -1,25 +1,16 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.opacity = "1.0";
-    
-    document.getElementsByClassName("container")[0].style.opacity = "0.0";
+var menu = $("#myNav");
+var crystal = $(".container");
+
+function openMenu() {
+	
+	menu.fadeTo("slow", 1);
+	crystal.fadeTo("slow", 0);
+	
 }
 
-<<<<<<< Updated upstream
-/* Open when someone clicks on the span element */
-function openFeat() {
-    document.getElementById("myNav").style.opacity = "0.0";
-    document.getElementsByClassName("yay").style.opacity = "1.0";
-}
-
-=======
->>>>>>> Stashed changes
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("myNav").style.opacity = "0.0";
-    document.getElementsByClassName("container")[0].style.opacity = "1";
-<<<<<<< Updated upstream
+	
+	menu.fadeTo("slow", 0);
+	crystal.fadeTo("slow", 1);
+	
 }
-=======
-}
->>>>>>> Stashed changes
